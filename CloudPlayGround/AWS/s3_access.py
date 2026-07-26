cbc6002306db04d4c9db81d83fa24a785d467ca8
mohtalha <mohtalha@gmail.com>
2026-07-26 18:08:39 -0400

    AWS  1st draft

--- CloudPlayGround/AWS/s3_access.py
+++ CloudPlayGround/AWS/s3_access.py
@@ -1,11 +1,17 @@
 import boto3
-#import dotenv
+from dotenv import load_dotenv
+import os
 
-dotenv.load_dotenv()
+load_dotenv()
+
+aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+region_name=os.getenv('AWS_DEFAULT_REGION')
+
 
 
 # Initialize S3 client
-s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_DEFAULT_REGION)
+s3 = boto3.client('s3', aws_access_key_id, aws_secret_access_key, region_name)
 
 # List all buckets
 def list_buckets():
@@ -28,4 +34,5 @@ def download_file(bucket_name, object_name, file_path):
     s3.download_file(bucket_name, object_name, file_path)
 
 if __name__ == '__main__':
-    print("Buckets:", list_buckets())
+    print(aws_access_key_id, aws_secret_access_key, region_name)
+    #    print("Buckets:", list_buckets())
